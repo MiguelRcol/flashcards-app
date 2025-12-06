905ff1107e025187022d8e185d55644b1009ad81
MiguelRcol <220428103+MiguelRcol@users.noreply.github.com>
2025-12-06 14:46:32 -0500

    Add flashcard struct and add_card flow

--- flashcards.c
+++ flashcards.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#define MAX_CARDS 100
+
+typedef struct{
+    char word[64];
+    char translation[64];
+    char language[16];
+} Flashcard;
 
 void show_menu(void);
+void add_card(Flashcard cards[], int *count);
 
 int main(void) {
     printf("Flashcards language app!\n");
-
+        Flashcard cards[MAX_CARDS];
+    int card_count = 0;
     int option = 0;
 
     do
@@ -17,7 +26,7 @@ int main(void) {
         switch (option)
         {
             case 1:
-                printf("Add new flashcard selected.\n");
+                add_card(cards, &card_count);
                 break;
             case 2:
                 printf("List flashcards selected.\n");
@@ -47,4 +56,24 @@ void show_menu() {
     printf("3. Quiz mode\n");
     printf("4. Save & Exit\n");
     printf("0. Exit without saving\n");
+}
+void add_card(Flashcard cards[], int *count) {
+    if (*count >= MAX_CARDS) {
+        printf("Flashcard limit reached!\n");
+        return;
+    }
+
+    Flashcard new_card;
+
+    printf("Enter word: ");
+    scanf("%s", new_card.word);
+    printf("Enter translation: ");
+    scanf("%s", new_card.translation);
+    printf("Enter language: ");
+    scanf("%s", new_card.language);
+
+    cards[*count] = new_card;
+    (*count)++;
+
+    printf("Flashcard added!\n");
 }
